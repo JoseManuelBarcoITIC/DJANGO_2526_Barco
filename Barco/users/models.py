a16c065641f8233b80ec7b61ea07ec16eca6f38e
JoseManuelBarcoITIC <2526_jose.barco@iticbcn.cat>
2026-03-09 20:12:03 +0100

    Realizado el models para Users

--- Barco/users/models.py
+++ Barco/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    f_name = models.CharField(max_length=30)
+    l_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    height = models.DecimalField(max_digits = 3, decimal_places=2)
+

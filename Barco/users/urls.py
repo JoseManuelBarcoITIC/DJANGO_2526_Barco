e99d830889073179f45451b982d372805b8832a4
JoseManuelBarcoITIC <2526_jose.barco@iticbcn.cat>
2026-03-19 19:54:18 +0100

    Casi acabado la practica, queda revisar i video

--- Barco/users/urls.py
+++ Barco/users/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('students/',views.getAllStudents ,name='select_students'),
     path('teachers/', views.getAllTeachers, name='select_teachers'),
+    path('create_user/', views.user_form, name='user_form'),
+    path('update_users/<int:pk>',views.update_user, name="update_u"),
+    path('delete_user/<int:pk>', views.delete_user, name="delete_u")
 
 ]
+
--- Barco/users/views.py
+++ Barco/users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from .models import User
+from .forms import UserForm
 
 
 def getAllStudents(request):
@@ -13,4 +14,33 @@ def getAllTeachers(request):
     response = {"persons":allTeachers}
     return render(request, 'main_page.html',response)
 
+def user_form(request):
+    form = UserForm()
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('select_students')
 
+    context = {"form": form}
+    return render(request,'form.html',context)
+
+def update_user (request,pk):
+    user_id = User.objects.get(id=pk)
+    form = UserForm(instance=user_id)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect ('select_students')
+
+    context = {"form":form}
+    return render(request, 'form.html',context)
+def delete_user (request,pk):
+    user_id = User.objects.get(id=pk)
+
+    if request.method == "POST":
+        user_id.delete()
+        return redirect ('select_students')
+    context = {"form":user_id}
+    return render(request, 'delete_user.html',context)
